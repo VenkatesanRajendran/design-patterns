868f08f4f0615274b829a0272c433f5f71102ee8
VenkatesanRajendran <82657767+VenkatesanRajendran@users.noreply.github.com>
2021-06-14 19:50:11 +0530

    Adapter and Decorator

---
+++ ConsoleApp2/Interface1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	public interface IInterface
+	{
+		void Add();
+		string GetString();
+	} 
+
+	internal class TestClass : IInterface
+	{
+		public void Add()
+		{
+			Console.WriteLine("A");
+		}
+
+		public string GetString()
+		{
+			return "B";
+		}
+
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
